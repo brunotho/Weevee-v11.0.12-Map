7c6a3c9ad8ca460621f57ea6ee76a5a6d92087da
brunotho <brunotho@users.noreply.github.com>
2026-09-09 17:15:06 +0200

    Bump mapscript identity to 11.0.7.

--- Weevee - v11.0.7.lua
+++ Weevee - v11.0.7.lua
@@ -12,7 +12,7 @@ include("DEFMultilayeredFractalW");
 include("DEFFeatureGeneratorW");
 include("DEFTerrainGeneratorW");
 
-print("Weevee Map 11.0.6 script loaded");
+print("Weevee Map 11.0.7 script loaded");
 
 local weeveeDbgHandle = nil;
 local WEEVEE_DBG_PATHS = {
@@ -62,7 +62,7 @@ function WeeveeDbgCall(name, fn, a1, a2, a3, a4, a5)
 		WeeveeDbg("ERR " .. name .. " " .. tostring(err));
 	end
 end
-WeeveeDbg("script loaded 11.0.6");
+WeeveeDbg("script loaded 11.0.7");
 
 local OPT_CENTER_SPLIT = 1;
 local OPT_FRONT_MOUNTAIN = 2;
@@ -117,7 +117,7 @@ end
 ------------------------------------------------------------------------------
 function GetMapScriptInfo()
 	return {
-		Name = "[COLOR_HIGHLIGHT_TEXT] Weevee Map 11.0.6 [ENDCOLOR]",
+		Name = "[COLOR_HIGHLIGHT_TEXT] Weevee Map 11.0.7 [ENDCOLOR]",
 		Description = "",
 		IsAdvancedMap = false,
 		SupportsMultiplayer = true,
